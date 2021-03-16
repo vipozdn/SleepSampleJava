cd4dbc092e892a2cbd937dc7ffeb93c6deaa3526
Victor Pozdnyakov <victorius152@gmail.com>
2021-03-16 11:55:24 +0300

    Create MainApplication class

--- app/src/main/java/com/lemon/sleepsamplejava/MainApplication.java
+++ app/src/main/java/com/lemon/sleepsamplejava/MainApplication.java
@@ -1,7 +1,26 @@
 package com.lemon.sleepsamplejava;
 
 import android.app.Application;
+import com.lemon.sleepsamplejava.data.datastore.SLEEP_PREFERENCES_NAME;
+
+
+import com.lemon.sleepsamplejava.data.db.SleepDatabase;
 
 public class MainApplication extends Application {
+
+    private SleepDatabase getDatabase() {
+        return SleepDatabase.getDatabase(getApplicationContext());
+    }
+
+    public SleepRepository getRepository() {
+        SleepRepository sleepRepository = new SleepRepository(
+                new SleepSubscriptionStatus(
+                        new RxPreferenceDataStoreBuilder(getApplicationContext(), SLEEP_PREFERENCES_NAME, "settings").build(),
+                        getDatabase().sleepSegmentEventDao(),
+                        getDatabase().sleepClassifyEventDao()
+                )
+        );
+        return sleepRepository;
+    }
 
 }
