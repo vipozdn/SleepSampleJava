cd31b14d691e60abe90a82d9970de4563a551f2a
Victor Pozdnyakov <victorius152@gmail.com>
2021-04-02 19:07:02 +0300

    Add repository methods to MainViewModel

--- app/src/main/java/com/lemon/sleepsamplejava/MainViewModel.java
+++ app/src/main/java/com/lemon/sleepsamplejava/MainViewModel.java
@@ -6,11 +6,20 @@ import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.lemon.sleepsamplejava.data.SleepRepository;
+import com.lemon.sleepsamplejava.data.db.SleepClassifyEventEntity;
+import com.lemon.sleepsamplejava.data.db.SleepSegmentEventEntity;
+
+import java.util.List;
 
 public class MainViewModel extends ViewModel {
 
     private SleepRepository sleepRepository;
     public LiveData<Boolean> subscribedToSleepDataLiveData = sleepRepository.subscribedToSleepLiveData;
+
+    public LiveData<List<SleepSegmentEventEntity>> allSleepSegments = sleepRepository.allSleepSegmentEvents;
+
+    public LiveData<List<SleepClassifyEventEntity>> allSleepClassifyEventEntities
+            = sleepRepository.allSleepClassifyEvents;
 
     public MainViewModel(SleepRepository sleepRepository) {
         this.sleepRepository = sleepRepository;
@@ -20,4 +29,5 @@ public class MainViewModel extends ViewModel {
         sleepRepository.updateSubscribedToSleepData(subscribed);
     }
 
+    
 }
